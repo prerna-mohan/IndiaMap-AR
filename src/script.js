f3e8abe3f3c2e2753b506fa0096551dd0d893c55
prerna-mohan <prerna271998@gmail.com>
2023-08-09 13:04:15 +0530

    audio changes

--- src/script.js
+++ src/script.js
@@ -7,29 +7,36 @@ const annotation = modelViewer.querySelector('.annotation');
 const info = modelViewer.querySelector('.info')
 const thanks = modelViewer.querySelector('.thanks')
 
-modelViewer.addEventListener('model-visibility', function() {
-    // if (iOS()) {
-    //     annotation.style.display = 'block'
-    // }
-    hoisted = true;
-    modelViewer.play({ repetitions: 1 });
-    sound.loop = true;
-    sound.play();
-    setTimeout(() => {
-        thanks.style.display = 'block'
-    },3500)
+modelViewer.addEventListener('model-visibility', function () {
+    if (iOS()) {
+        startAnimations();
+    }
+    sound.play().catch(e => {
+        window.addEventListener('click', () => {
+           sound.play()
+        })
+     })
 })
 
-// modelViewer.addEventListener('ar-status', (event) => {
-//     if (event.detail.status == 'session-started' && !hoisted) {
-//         setTimeout(() => {
-//             info.style.display = 'block'
-//         }, 2000)
-//         setTimeout(() => {
-//             annotation.style.display = 'block'
-//         }, 7000)
-//     }
-// })
+modelViewer.addEventListener('ar-status', (event) => {
+    if (event.detail.status == 'session-started' && !hoisted) {
+        startAnimations()
+    }
+})
+
+function startAnimations(){
+    hoisted = true;
+    modelViewer.play({ repetitions: 1 });
+    // sound.loop = true;
+    // sound.play().catch(e => {
+    //     window.addEventListener('click', () => {
+    //        sound.play()
+    //     })
+    //  })
+    setTimeout(() => {
+        thanks.style.display = 'block'
+    }, 19000)
+}
 
 modelViewer.addEventListener('load', () => {
     console.log('model loaded')
@@ -38,3 +45,16 @@ modelViewer.addEventListener('load', () => {
 annotation.addEventListener('click', (e) => {
     console.log('clicked', e.target)
 })
+
+function iOS() {
+    return [
+      'iPad Simulator',
+      'iPhone Simulator',
+      'iPod Simulator',
+      'iPad',
+      'iPhone',
+      'iPod'
+    ].includes(navigator.platform)
+    // iPad on iOS 13 detection
+    || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+  }
