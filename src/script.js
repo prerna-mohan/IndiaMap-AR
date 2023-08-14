8ccbadae676f059c149a607b161099a157d231f8
prerna-mohan <prerna271998@gmail.com>
2023-08-14 11:00:08 +0530

    code change

--- src/script.js
+++ src/script.js
@@ -8,7 +8,8 @@ let sound = new Howl({
   });
 const modelViewer = document.getElementById('mv');
 const annotation = modelViewer.querySelector('.annotation');
-const text1 = modelViewer.querySelector('.text1')
+const text1 = modelViewer.querySelector('.text1');
+const text2 = modelViewer.querySelector('.text2');
 
 modelViewer.addEventListener('model-visibility', function () {
     if (iOS()) {
@@ -36,6 +37,7 @@ function startAnimations(){
     setTimeout(() => {
         text1.style.display = 'none'
         annotation.style.display = 'block'
+        text2.style.display = 'block';
     }, 34000)
 }
 
